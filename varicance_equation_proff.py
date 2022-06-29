fed2480188d747995b6ac2b8459ec4a339b438c9
shoabahamed <105428244+shoabahamed@users.noreply.github.com>
2022-06-29 13:58:50 +0600

    updated

--- varicance_equation_proff.py
+++ varicance_equation_proff.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
-# Here we are reading a text which gives us list of values which represents how much water 
-# 1000 people takes every day(the data is not real and was generate though random moudle in
+# Here we are reading a text which gives us list of values which represents age 
+# 5000 people (the data is not real and was generate though random moudle in
 # python)
+
 with open('daily_water_intake.txt', 'r') as file:
     values = file.readlines()
     population = [int(value.strip()) for value in values]
 
 # first lets convert the list to numpy array for fast and easy calculation
 population_array = np.array(population)
+# population_array = np.array(population)
 # now lets write a function which would give us the variance
 def get_variance(values, divider):
     mean = np.mean(values)
@@ -23,14 +24,24 @@ population_variance = get_variance(population_array, len(population_array))
 # now lets take the mean of the population
 population_mean = np.mean(population)
 
-for i in range(1000):
-    sample = np.random.choice(population_array, size=20, replace=False)
+for i in range(20, 500):
+    # size = random.randint(50, 500)
+    size = 20
+    sample = np.random.choice(population_array, size=i, replace=False)
     sample_mean = np.mean(sample)
-    sample_variance = get_variance(sample, len(sample)-1)
-    plt.scatter(sample_mean, sample_variance, color='blue', edgecolor='black')
+    sample_variance = get_variance(sample, len(sample))
+    size += 1
 
-plt.xlim((0, 10))
-plt.ylim(0, 15)
+    if i > 200:
+        color = 'blue'
+    else: 
+        color = 'red'
+
+    plt.scatter(sample_mean, sample_variance, color=color, edgecolor='black')
+
+
+plt.xlim((15, 80))
+plt.ylim(200, 500)
 
 plt.axhline(population_variance, color='black', ls='--')
 plt.axvline(population_mean, color='black', ls='--')
